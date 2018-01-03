1f2d30b481006d74f84d7004bfc4d7710d656e3c
Petya Djanovska <petya.djanovska@gmail.com>
2018-01-03 19:02:15 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,8 +8,9 @@ def summon_captain_planet(collection)# code an argument here
   collection.map {|item| "#{item.capitalize}!"}
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls (collection)# code an argument here
   # Your code here
+  collection.any? { |call| call.length > 4 }
 end
 
 def find_the_cheese# code an argument here
